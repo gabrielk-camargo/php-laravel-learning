20049f22d562cd02494d43d2509e78dc73a68a2f
Gabriel Kist Camargo <gabrielkcamargo0@hotmail.com>
2025-06-17 15:14:37 -0300

    feat: New Study

--- main.php
+++ main.php
@@ -4,9 +4,16 @@ echo  "Bem vindo(a) ao screen match \n";
 
 $nameFilme = $argv[1] ?? "Não Consta";
 $yearOfRelease = $argv[2] ?? "Não Consta";
-$sumNotes = (3 + 3 + 9 + 2 + 2);
-$filmeReview = $sumNotes/5 ;
+$notesAmount = $argc - 3;
+$sumNotes = 0;
 $includedInIhePlan = true;
+
+for($amount = 3; $amount < $argc; $amount ++){
+    $sumNotes += $argv[$amount];
+}
+
+$filmeReview = $sumNotes/$notesAmount;
+
 $classFilme = match ($nameFilme){
     "Thor" => "Aventura",
     "Interestelar" => "Ficção",
@@ -14,5 +21,12 @@ $classFilme = match ($nameFilme){
     default => "Não consta"
 };
 
+if ($yearOfRelease <= 2022) {
+    echo "Esse filme teve lançamento a \n" . 2025-$yearOfRelease . " anos então ele é velho \n";
+} elseif ($yearOfRelease >= 2023 && $yearOfRelease <= 2024) {
+    echo "Esse filme teve lançamento a \n" . 2025-$yearOfRelease . " anos então ele é recente \n";
+} else {
+    echo "Esse filme é lançamento\n";
+}
 
-echo "O nome do filme é " . $nameFilme . "\n" . "O ano de lançamento é " . $yearOfRelease . "\n" . "Sua avalição é " . $filmeReview . "\n" . "Gênero do filme é " . $classFilme;
+echo "O nome do filme é " . $nameFilme . "\n" . "O ano de lançamento é " . $yearOfRelease . "\n" . "Sua avalição é " . number_format($filmeReview, 1, ',', '.') . "\n" . "Gênero do filme é " . $classFilme;
